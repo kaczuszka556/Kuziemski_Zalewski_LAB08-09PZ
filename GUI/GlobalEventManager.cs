7fad160f5e1d5c503e41723f8bee8bd398cc0827
Maciej Zalewski <maciej.zalewski@redocean.pl>
2023-06-10 10:10:31 +0200

    Event manager

---
+++ GUI/GlobalEventManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    internal static class GlobalEventManager
+    {
+
+        public delegate void LanguageChange();
+        public delegate void EventCalendarChanged();
+
+        public static event LanguageChange OnLanguageChanged;
+        public static event EventCalendarChanged OnEventCalendarChanged;
+
+        public static void TriggerOnLanguageChanged()
+        {
+            OnLanguageChanged?.Invoke();
+        }
+
+        public static void TriggerOnEventCalendarChanged()
+        {
+            OnEventCalendarChanged?.Invoke();
+        }
+
+    }
+}
